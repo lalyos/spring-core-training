cd9d0c0294ef97a59d4fa6f8ca0450edd58cfd90
lajos papp <lalyos@yahoo.com>
2012-08-30 11:52:23 +0200

    new property is added

--- src/main/java/com/github/acme/spring/SimpleGreetingService.java
+++ src/main/java/com/github/acme/spring/SimpleGreetingService.java
@@ -2,9 +2,37 @@ package com.github.acme.spring;
 
 public class SimpleGreetingService implements GreetingService {
 
-    public void sayGreeting() {
-        System.out.println("Hello Service!");
+    private String msg = "Hello Service!";
+    private int count;
+    
+    public SimpleGreetingService(){};
+    
+    public SimpleGreetingService(String msg, int count) {
+        this.msg = msg;
+        this.count = count;
+    }
 
+    public void sayGreeting() {
+        for(int i=0 ; i < count; i++) {
+            System.out.println(msg);
+        }
+
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
     }
 
 }
