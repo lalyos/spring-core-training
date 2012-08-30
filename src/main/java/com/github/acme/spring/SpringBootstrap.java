a931007bcdd59753ce09c5047448e885af7e00a9
lajos papp <lalyos@yahoo.com>
2012-08-30 14:15:23 +0200

    switch from BeanFactory to ApplicationContext

--- src/main/java/com/github/acme/spring/SpringBootstrap.java
+++ src/main/java/com/github/acme/spring/SpringBootstrap.java
@@ -1,15 +1,17 @@
 package com.github.acme.spring;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import java.util.Locale;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringBootstrap {
 
     public static void main(String[] args) {
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src/main/resources/META-INF/spring/application.xml"));
-        GreetingApp greetingApp = factory.getBean(GreetingApp.class);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/spring/*.xml");
+        GreetingApp greetingApp = ctx.getBean(GreetingApp.class);
         greetingApp.greet();
+        
     }
 
 }
