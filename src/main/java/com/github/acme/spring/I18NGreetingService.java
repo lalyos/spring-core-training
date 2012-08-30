1f417f33418ccb88efdd38aaf82ba16d04661592
lajos papp <lalyos@yahoo.com>
2012-08-30 15:15:14 +0200

    multiple message source (basenames)

--- src/main/java/com/github/acme/spring/I18NGreetingService.java
+++ src/main/java/com/github/acme/spring/I18NGreetingService.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.NoSuchMessageException;
 
 public class I18NGreetingService implements GreetingService, ApplicationContextAware {
 
@@ -12,8 +13,14 @@ public class I18NGreetingService implements GreetingService, ApplicationContextA
     private ApplicationContext ctx;
     
     public void sayGreeting() {
-        System.out.println(String.format("[%s] :  %s"
-                , locale.getDisplayLanguage(), ctx.getMessage("msg", null, locale)  ));
+        String key = "__msg";
+        try {
+            
+            System.out.println(String.format("[%s] :  %s"
+                , locale.getDisplayLanguage(), ctx.getMessage(key, null, locale)  ));
+        }catch (NoSuchMessageException e) {
+            System.out.println(ctx.getMessage("error.nosuchmessage", new Object[]{key}, locale));
+        }
 
     }
 
