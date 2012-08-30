0335cb066b07469d088ce2e9a4dca38aa8dc8c54
lajos papp <lalyos@yahoo.com>
2012-08-30 10:58:59 +0200

    springified with BeanFactory

--- src/main/java/com/github/acme/spring/GreetingApp.java
+++ src/main/java/com/github/acme/spring/GreetingApp.java
@@ -1,9 +1,13 @@
 package com.github.acme.spring;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 public class GreetingApp {
     public static void main(String[] args) {
-        GreetingService service = new SimpleGreetingService();
-        
+        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src/main/resources/META-INF/spring/application.xml"));
+        GreetingService service = (GreetingService) factory.getBean("greetingService");
         service.sayGreeting();
     }
 }
